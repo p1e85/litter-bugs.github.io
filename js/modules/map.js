d701fb54aeeb02f7ef92c61f0424da843e6b8376
patrick d. <p1e85@users.noreply.github.com>
2025-10-17 08:04:27 -0500

    Update map.js

--- js/modules/map.js
+++ js/modules/map.js
@@ -22,14 +22,19 @@ export function initializeMap() {
         proximity: 'ip', // Prioritize results near the user's IP address
         types: 'country,region,place,postcode,locality,neighborhood,address,poi' // Expand search to include POIs
 
-        // for chicago search only if want to implement
-        //bbox: [-87.9401, 41.6445, -87.5241, 42.0231], // Chicago Bounding Box
-        //proximity: { // Proximity is now set to a point inside Chicago
-        //    longitude: -87.6298,
-        //    latitude: 41.8781
-        //}
     });
     document.getElementById('geocoder-container').appendChild(geocoder.onAdd(state.map));
+
+    const searchInput = document.querySelector('#geocoder-container .mapboxgl-ctrl-geocoder--input');
+
+    // This code makes the input readonly initially, then removes that attribute
+    // as soon as the user focuses on it (by clicking or tabbing).
+    if (searchInput) {
+        searchInput.setAttribute('readonly', 'readonly');
+        searchInput.onfocus = () => {
+            searchInput.removeAttribute('readonly');
+        };
+    }
 
     state.map.on('load', () => {
         initializeMapLayers();
