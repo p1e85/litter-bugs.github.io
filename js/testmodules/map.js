7171061fb7ded126b7f7cca376037f99f00df9ad
patrick d. <p1e85@users.noreply.github.com>
2026-01-25 11:52:52 -0600

    Update map.js

--- js/testmodules/map.js
+++ js/testmodules/map.js
@@ -1,5 +1,5 @@
 import { state, mapStyles, ZOOM_THRESHOLD } from './config.js';
-import { fetchAndDisplayCommunityRoutes, setupPoiClickListeners, showPublicProfile } from './community.js';
+import { fetchAndDisplayCommunityRoutes, setupPoiClickListeners} from './community.js';
 import { pinCategories } from './config.js';
 import { showPublicProfile } from './ui.js';
 
