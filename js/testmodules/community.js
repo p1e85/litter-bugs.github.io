0ac35e67e2ad8789eb80954faa43b7eb4237580a
patrick d. <p1e85@users.noreply.github.com>
2026-02-03 22:10:49 -0600

    Update community.js

--- js/testmodules/community.js
+++ js/testmodules/community.js
@@ -1,7 +1,7 @@
 import { 
     db, serverTimestamp, Timestamp, collection, getDocs, query, orderBy, addDoc, doc, getDoc, where, setDoc, deleteDoc, updateDoc, onSnapshot, limit, storage, ref, uploadBytes, getDownloadURL, runTransaction 
 } from './firebase.js';
-import { state, allBadges, allTitles, profanityList } from './config.js';
+import { state, allBadges, allTitles, profanityList, RP_SECTORS } from './config.js';
 import { convertRouteForFirestore, convertPinsForFirestore, convertRouteFromFirestore, convertPinsFromFirestore } from './utils.js';
 import { clearCurrentSession } from './data.js';
 import { showPublicProfile } from './ui.js';
