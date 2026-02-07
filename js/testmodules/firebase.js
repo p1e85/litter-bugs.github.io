1e73ac5a0266813a331a136521a2bd8b63f33c45
patrick d. <p1e85@users.noreply.github.com>
2026-02-07 15:51:57 -0600

    Update firebase.js

--- js/testmodules/firebase.js
+++ js/testmodules/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { 
     getFirestore, doc, getDoc, setDoc, updateDoc, collection, increment, arrayRemove,
-    addDoc, getDocs, query, orderBy, where, deleteDoc, limit, onSnapshot, serverTimestamp, Timestamp, arrayUnion, arrayRemove, runTransaction
+    addDoc, getDocs, query, orderBy, where, deleteDoc, limit, onSnapshot, serverTimestamp, Timestamp, arrayUnion, runTransaction
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { 
     getStorage, ref, uploadBytes, getDownloadURL 
